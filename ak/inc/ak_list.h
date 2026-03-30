e0735b9c2a3dbe94524e3507d8b6b8e99dfef0d4
Mab-Nig <maivuphong4@gmail.com>
2026-03-30 16:35:09 +0700

    Done: ak_list.

--- ak/inc/ak_list.h
+++ ak/inc/ak_list.h
@@ -11,10 +11,15 @@ struct __ak_list_node {
   ak_list_node_t *next;
 };
 
-/** Insert new_node before pos. Insert at end if pos is NULL.
+/** Insert new_node before pos.
  * @return new_node if success. NULL if not.
  */
-ak_list_node_t *ak_list_ins(ak_list_node_t *pos, ak_list_node_t *new_node);
+ak_list_node_t *ak_list_ins_bef(ak_list_node_t *pos, ak_list_node_t *new_node);
+
+/** Insert new_node after pos.
+ * @return new_node if success. NULL if not.
+ */
+ak_list_node_t *ak_list_ins_aft(ak_list_node_t *pos, ak_list_node_t *new_node);
 
 /** Remove node at pos.
  * @return pos->next if success. NULL if not.
